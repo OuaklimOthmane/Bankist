57374da78ac2f788e3aedacce11fb663b501f6f0
Ouaklim Othmane <ouaklimothmane@gmail.com>
2021-12-03 00:07:13 +0100

    Display movments fixed

--- script.js
+++ script.js
@@ -59,9 +59,12 @@ const inputClosePin = document.querySelector(".form__input--pin");
 
 /////////////////////////////////////////////////
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = "";
-  movements.forEach(function (movement, index) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (movement, index) {
     const type = movement > 0 ? "deposit" : "withdrawal";
     const html = `<div class="movements__row">
               <div class="movements__type movements__type--${type}">${
