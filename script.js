880d0aeac8952843558adc48dc721fd9f1836e0f
Ouaklim Othmane <ouaklimothmane@gmail.com>
2021-12-06 21:49:20 +0100

    Rounding Balance,loan amount, incomes, outcomes, movements,interests

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -13,6 +25,18 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account3 = {
@@ -70,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
               <div class="movements__type movements__type--${type}">${
       index + 1
     } ${type}</div>
-              <div class="movements__value">${movement}€</div>
+              <div class="movements__value">${movement.toFixed(2)}€</div>
             </div>`;
 
     containerMovements.insertAdjacentHTML("afterbegin", html);
@@ -94,7 +118,7 @@ const calcDisplayBalance = function (account) {
     (accumulator, movement) => accumulator + movement,
     0
   );
-  labelBalance.textContent = `${account.balance}€`;
+  labelBalance.textContent = `${account.balance.toFixed(2)}€`;
 };
 calcDisplayBalance(account1);
 
@@ -102,19 +126,19 @@ const calcDisplaySummary = function (account) {
   const incomes = account.movements
     .filter((movement) => movement > 0)
     .reduce((accumulator, movement) => accumulator + movement, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = account.movements
     .filter((movement) => movement < 0)
     .reduce((accumulator, movement) => accumulator + movement, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = account.movements
     .filter((movement) => movement > 0)
     .map((deposite) => (deposite * account.interestRate) / 100)
     .filter((deposite) => deposite >= 1)
     .reduce((accumulator, deposite) => accumulator + deposite, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 calcDisplaySummary(account1);
 
@@ -195,7 +219,7 @@ btnTransfer.addEventListener("click", function (e) {
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
   if (
     amount > 0 &&
     currentAccount.movements.some((movement) => movement >= amount * 0.1)
